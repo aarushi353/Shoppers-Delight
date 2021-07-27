2f607c3c16e46ba0c8322396338d928b724c673e
aarushi353 <aarushiabrol999@gmail.com>
2021-07-27 16:37:55 +0530

    added material ui icons package

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Landing.css";
 import image from "../assets/images/landing.png";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 function LandingPage() {
   return (
     <div className="Landing">
       <center>
         <strong>
-          <p>Your Online Shopping Destination</p>
+          <p>
+            Your Online Shopping Destination <ShoppingCartIcon />
+          </p>
         </strong>
       </center>
       <center>
