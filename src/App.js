458610cd6f672191664d2811fe7db7a1f3d17ed1
aarushi353 <aarushiabrol999@gmail.com>
2021-07-27 14:13:18 +0530

    basic routing completed and navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
-
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          Landing Page
+        </Route>
+        <Route path="/Search" exact>
+          Search View
+        </Route>
+        <Route path="/Products" exact>
+          Products
+        </Route>
+        <Route path="/Cart" exact>
+          Cart
+        </Route>
+        <Route path="/Checkout" exact>
+          Checkout
+        </Route>
+        <Route path="/Login" exact>
+          Login
+        </Route>
+        <Route path="/Signup" exact>
+          SignUp
+        </Route>
+        <Route path="/Admin" exact>
+          Admin Page
+        </Route>
+      </Switch>
     </div>
   );
 }
