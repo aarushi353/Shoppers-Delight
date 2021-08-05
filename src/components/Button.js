00a5ad28d108cb2763d1f861d857f19ca688b7bd
aarushi353 <aarushiabrol999@gmail.com>
2021-08-05 21:11:01 +0530

    unwanted files deleted

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Buttons.css';
-function Button(props){
-    return(
-        <button className="button">{props.text}</button>
-    )
-}
-
-export default Button;
