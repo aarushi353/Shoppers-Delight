602c084335b30f0a0bd68ae3b4fe1a6d45330977
Ishaan-10 <b.ishaan10@gmail.com>
2021-09-06 22:14:19 +0530

    6/09/2021 progress

--- server/Models/products.js
+++ server/Models/products.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name:{
+    title:{
         type:String,
         required:true
     },
@@ -13,6 +13,11 @@ const productSchema = new mongoose.Schema({
         type:Number,
         required:true,
     },
+    category:{
+        type:String,
+        required:true
+    },
+    images:[{url:String}],
     isDiscounted:{
         type:Boolean,
         default:false,
